4ba3735732630cf0be340fe77dd144411e064933
Ph4sm <51958370+Ph4sm@users.noreply.github.com>
2023-04-19 19:45:10 +0200

    Update index.js

--- Code/index.js
+++ Code/index.js
@@ -1,16 +1,26 @@
 const { getLastEmailContent } = require('./gmail');
-const { createPage } = require('./notion');
+const { createPage, getLastDate } = require('./notion');
 
+async function getDate() {
+  global.lastDate = await getLastDate();
+  //console.log(`Last date in database: ${global.lastDate}`);
+}
+
+getDate();
 
 setInterval(async () => {
+  
   const { subject, content, type, dateTime } = await getLastEmailContent();
-  if (subject && content) {
+  //console.log(`Date and time: ${dateTime}`);
+  if (dateTime > global.lastDate) {
     console.log(`New mail received: ${subject}`);
     console.log(`Mail content: ${content}`);
     console.log(`Message type: ${type}`);
     console.log(`Date and time: ${dateTime}`);
-    await createPage({ title: subject, content: content, type: type });
+    await createPage({ title: subject, content: content, type: type, dateTime: dateTime });
+    global.lastDate = dateTime;
+
   } else {
     console.log('No new mail found.');
   }
-}, 10000);
+}, 60000);
