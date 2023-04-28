1ec54b6180d69f65173789042ec51b685610211e
Ph4sm <51958370+Ph4sm@users.noreply.github.com>
2023-04-28 13:03:20 +0200

    Update calendar.js

--- Code/calendar.js
+++ Code/calendar.js
@@ -15,40 +15,47 @@ oauth2Client.setCredentials({ access_token: ACCESS_TOKEN, refresh_token: REFRESH
 const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
 
 async function createEvent(title, date, length) {
-  let endDateTime = date;
+  
+  try {
+    let endDateTime = date;
 
-  if (length) {
-    const hoursMatch = length.match(/(\d+)h/);
-    const minutesMatch = length.match(/(\d+)m/);
-    const hours = hoursMatch ? parseInt(hoursMatch[1]) : 0;
-    const minutes = minutesMatch ? parseInt(minutesMatch[1]) : 0;
+    if (length) {
+      const hoursMatch = length.match(/(\d+)h/);
+      const minutesMatch = length.match(/(\d+)m/);
+      const hours = hoursMatch ? parseInt(hoursMatch[1]) : 0;
+      const minutes = minutesMatch ? parseInt(minutesMatch[1]) : 0;
 
-    endDateTime = new Date(new Date(date).getTime() + hours * 60 * 60 * 1000 + minutes * 60 * 1000);
-  }
-
-  const event = {
-    summary: title,
-    start: {
-      dateTime: date,
-      timeZone: 'Europe/Paris',
-    },
-    end: {
-      dateTime: endDateTime,
-      timeZone: 'Europe/Paris',
-    },
-  };
-
-  // Insertion de l'événement dans le calendrier
-  calendar.events.insert({
-    calendarId: 'primary',
-    resource: event,
-  }, (err, res) => {
-    if (err) {
-      console.error('Error creating event:', err);
-      return;
+      endDateTime = new Date(new Date(date).getTime() + hours * 60 * 60 * 1000 + minutes * 60 * 1000);
     }
-    console.log('Event created: %s', res.data.htmlLink);
-  });
+
+    const event = {
+      summary: title,
+      start: {
+        dateTime: date,
+        timeZone: 'Europe/Paris',
+      },
+      end: {
+        dateTime: endDateTime,
+        timeZone: 'Europe/Paris',
+      },
+    };
+
+    // Insertion de l'événement dans le calendrier
+    calendar.events.insert({
+      calendarId: 'primary',
+      resource: event,
+    }, (err, res) => {
+      if (err) {
+        console.error('Error creating event:', err);
+        return;
+      }
+      console.log('Event created: %s', res.data.htmlLink);
+    });
+    
+  } catch (err) {
+    console.error("Error creating event:", err);
+    return null;
+  }
 }
 
 module.exports = { createEvent };
